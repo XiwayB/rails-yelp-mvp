c411353808cfb6b5de128080b81ca67a6a10597d
XiwayBanh <xiway.banh@gmail.com>
2021-03-31 13:51:23 +0800

    added restaurants and reviews controllers and views"

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w(Chinese Italian Japanese French Belgian)
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, presence: true
-  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian) }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: "restaurants#index"
+resources :restaurants, only: [ :index, :new, :create, :show] do
+    resources :reviews, only: [ :create ]
+  end
 end
+  # RESTAURANTS
+  # See all restaurants // Index
+  # Create a new restaurant // New + Create
+  # See a restaurant // Show
+
+  # REVIEWS
+  # Create a new review // New + Create
+  # See reviews in restaurant
